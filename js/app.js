701036b95be8eb70ecb37249fb5a7eab16c7d931
IanLockwood <mrianlockwood@gmail.com>
2016-02-16 16:52:23 -0500

    Add JQ for about page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation();
+
+$(document).ready(function() {
+	$(".about-link").on("click", function() {
+		if ($("#about").is(":hidden")) {
+			$(".paper:visible").fadeOut("slow", function() {
+				$("#about").fadeIn("slow");
+			});
+		};		
+	})
+});
+
+
+1 800 222 0446
+
+4448
+
+158887755
